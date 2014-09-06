a710c4d05948a10e751707a843acc9a201fda5f3
LegendLukaz <lukasfan08@gmail.com>
2014-09-06 13:06:22 +0800

    Added proxy and mod class

---
+++ Source/Main/java/com/legendlukaz/machinecraftreboot/MachineCraftReboot.java
@@ -0,0 +1,31 @@
+package com.legendlukaz.machinecraftreboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "MachineCraftReboot", name = "MachineCraft Reboot", version = "1.7.2-1.0")
+public class MachineCraftReboot
+{
+    @Mod.Instance("MachineCraftReboot")
+    public static MachineCraftReboot instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
